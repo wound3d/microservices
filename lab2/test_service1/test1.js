302057cec02ac515c90cc7d64f6e159d7326f293
Emil Denishev <emelya2003@mail.ru>
2025-11-03 10:59:51 +0700

    fix routing + added logging

--- lab2/test_service1/test1.js
+++ lab2/test_service1/test1.js
@@ -1,6 +1,6 @@
-// import express from 'express';
-// import logger from './logger.js';
-// import { v4 as uuidv4 } from 'uuid';
+import express from 'express';
+import logger from './logger.js';
+import { v4 as uuidv4 } from 'uuid';
 
 const express = require('express')
 const uuidv4 = require('uuid')
@@ -8,21 +8,18 @@ const uuidv4 = require('uuid')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
+app.get('/service1', (req, res) => {
   res.send('Service 1 got your request!')
-  const requestID = req.headers['x-request-id']
-  // logger.info(`Service1: Request with id ${requestID} received!`)
-    console.log(`Service1: Request with id ${requestID} received!`)
+  const requestID = req.headers['x-request-id'] || uuidv4()
+  logger.info(`Service1: Request with id ${requestID} received!`)
 })
 
-app.get('/test', (req, res) => {
+app.get('/service1/test', (req, res) => {
   res.send('Service 1 Test got your request!')
   const requestID = req.headers['x-request-id']
-  // logger.info(`Service1: Request with id ${requestID} received!`)
-  console.log(`Service1: Request with id ${requestID} received!`)
+  logger.info(`Service1: Request with id ${requestID} received!`)
 })
 
 app.listen(port, () => {
-  //logger.info(`Service 1 has started on port ${port}`)
-  console.log(`Service 1 has started on port ${port}`)
+  logger.info(`Service 1 has started on port ${port}`)
 })
